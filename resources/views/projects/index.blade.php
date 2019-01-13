e92b202cd07342f3477e4353805b33c200180bb4
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2019-01-13 17:51:59 -0600

    :art: Start implementing frontend with TailwindCSS

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,11 +3,16 @@
 <div class="flex items-center mb-3">
     <a href="/projects/create">New Project</a>
 </div>
+<div class="flex">
 @forelse ($projects as $project)
-<li>
-    <a href="{{ route('projects.show',$project) }}">{{ $project->title }}</a>
-</li>
-@empty
-<li>No projects yet.</li>
-@endforelse
+    <div class="bg-white w-1/3 p-4 mr-4 rounded shadow h-48">
+        <h3 class="font-normal text-xl mb-6">{{ $project->title }}</h3>
+        <div class="text-black opacity-40">
+            {{ str_limit($project->description, 100) }}
+        </div>
+    </div>
+    @empty
+    <div>No projects yet.</div>
+    @endforelse
+</div>
 @endsection
